43e67b8faa61596d55f5c7e3f3f6d410cd14b61f
Mauro O. Favre <89173092+Maurofavre@users.noreply.github.com>
2025-11-12 00:16:35 -0300

    arreglos

--- modelo-6/01/index.mjs
+++ modelo-6/01/index.mjs
@@ -2,8 +2,8 @@
 import {writeFile} from 'fs/promises'
 import path from 'path'
 import { stringify } from 'querystring'
-const productos ={
 
+const productos ={
     nombre:'AURICULARES DE PRUEBA' ,
     marca:'MAUROO' ,
     precio: '15000' 
@@ -11,18 +11,15 @@ const productos ={
 
 const nuevoJson = path.resolve('producto.json')
 
-
-
 async function nuevoArchivo() {
     try {
-     
         
-    const nuevoProductos = JSON.stringify(productos)
-
-    await writeFile(nuevoJson,nuevoProductos)
-
+        const nuevoProductos = JSON.stringify(productos)
+         await writeFile(nuevoJson,nuevoProductos)
+        console.log('✅ Archivo creado correctamente:', nuevoJson)
+        
     } catch (error) {
-        
+        console.error('❌ Error al crear el archivo:', error.message)
     }
 
 }
